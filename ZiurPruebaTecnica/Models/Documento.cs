68cf4fa676c159cbf3393c9b279d4ab0b4a18251
Esteban Lopez <estebanlopezparada.dev@gmail.com>
2026-01-31 22:12:21 -0500

    Entrega final del proyecto

---
+++ ZiurPruebaTecnica/Models/Documento.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZiurPruebaTecnica.Models
+{
+    public class Documento
+    {
+
+        public int Codigo { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public bool VActiva { get; set; }
+
+    }
+}
---
+++ ZiurPruebaTecnica/Services/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Net.Http.Headers;
+using ZiurPruebaTecnica.Models;
+using static System.Net.WebRequestMethods;
+
+namespace ZiurPruebaTecnica.Services;
+
+public class DataService(HttpClient http)
+{
+    public async Task<List<Documento>> GetDocumentosAsync()
+    {
+
+        var token = "ae8bad44-7348-11ee-b962-0242ac120002";
+        http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        var url = "https://mainserver.ziursoftware.com/Ziur.API/basedatos_01/ZiurServiceRest.svc/api/DocumentosFillsCombos";
+        var response = await http.GetFromJsonAsync<List<Documento>>(url);
+
+        return response ?? new List<Documento>();
+    }
+}
